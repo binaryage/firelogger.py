be96d11c776bb2d85cd1ed9de9b24c730092230b
Alexander Solovyov <piranha@piranha.org.ua>
2008-11-24 17:54:00 +0200

    wsgi middleware

---
+++ python/firepython/wsgi.py
@@ -0,0 +1,37 @@
+# WSGI support for FirePython
+
+import logging
+import firepython
+
+class FirePythonWSGI(object):
+    """
+    WSGI middleware to enable FirePython logging.
+    """
+    def __init__(self, app):
+        self.app = app
+        self.handler = firepython.FirePythonLogHandler()
+        self.root = logging.getLogger()
+
+    def __call__(self, environ, start_response):
+        self.root.addHandler(self.handler)
+        self.root.setLevel(logging.DEBUG)
+
+        # collect headers
+        resp_info = []
+        def faked_start_response(status, headers, exc_info=None):
+            resp_info.append(status)
+            resp_info.append(headers)
+            resp_info.append(exc_info)
+
+        # run app
+        app_iter = self.app(environ, faked_start_response)
+
+        # collect logs
+        def add_header(name, value):
+            resp_info[1].append((name, value))
+        self.root.removeHandler(self.handler)
+        self.handler.flush(add_header)
+
+        # start responding
+        start_response(*resp_info)
+        return app_iter
