44926672b67a94ce177fcae5a06b28d5d92be35d
Alexander Solovyov <piranha@piranha.org.ua>
2008-11-24 20:08:05 +0200

    setup.py for easy_installability

---
+++ python/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='firepython',
+      version='0.2',
+      description='Python logging to firebug',
+      author='Antonin Hildebrand',
+      author_email='antonin@hildebrand.cz',
+      url='http://github.com/woid/firepython/tree',
+      packages=['firepython'],
+      classifiers=['Development Status :: 1',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Software Development :: Bug Tracking',
+                   'Topic :: Software Development :: Quality Assurance',
+                   'Topic :: Software Development :: Testing',
+                   'Topic :: System :: Logging'],
+      )
