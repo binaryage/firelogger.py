1196cb93403a84f0926bb4a5e85a1a843deaad29
Dan Buch <daniel.buch@gmail.com>
2009-07-26 15:52:35 -0400

    slight mods to rakefile for when in development mode

--- setup.py
+++ setup.py
@@ -1,25 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import sys
+from setuptools import setup
 
-from distutils.core import setup
+try:
+    from firepython import __version__
+except ImportError:
+    print >> sys.stderr, """\
+        If developing firepython from the source checkout,
+        one must first run `rake develop`, or simply create
+        a `firepython` dir and copy all but this `setup.py`
+        python module into it.
+    """
+    sys.exit(1)
 
-from firepython import __version__
 
-setup(name='firepython',
-      version=__version__,
-      description='Python logging console integrated into Firebug',
-      author='Antonin Hildebrand',
-      author_email='antonin@hildebrand.cz',
-      url='http://firepython.binaryage.com',
-      packages=['firepython'],
-      classifiers=['Development Status :: 4 - Beta',
-                   'Environment :: Web Environment',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: BSD License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Topic :: Software Development :: Bug Tracking',
-                   'Topic :: Software Development :: Quality Assurance',
-                   'Topic :: Software Development :: Testing',
-                   'Topic :: System :: Logging'],
-      )
+SETUP_ARGS = dict(
+    name='firepython',
+    version=__version__,
+    description='Python logging console integrated into Firebug',
+    author='Antonin Hildebrand',
+    author_email='antonin@hildebrand.cz',
+    url='http://firepython.binaryage.com',
+    packages=['firepython'],
+    classifiers=['Development Status :: 4 - Beta',
+                'Environment :: Web Environment',
+                'Intended Audience :: Developers',
+                'License :: OSI Approved :: BSD License',
+                'Operating System :: OS Independent',
+                'Programming Language :: Python',
+                'Topic :: Software Development :: Bug Tracking',
+                'Topic :: Software Development :: Quality Assurance',
+                'Topic :: Software Development :: Testing',
+                'Topic :: System :: Logging'],
+)
+
+def main():
+    setup(**SETUP_ARGS)
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
