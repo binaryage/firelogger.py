3ba1dd60078426b922f04aa32bb23ddd36c837c4
Dan Buch <daniel.buch@gmail.com>
2009-08-28 23:46:44 -0400

    firepython/__init__.py: updating docstring since README renaming

--- firepython/__init__.py
+++ firepython/__init__.py
@@ -1,10 +1,10 @@
 # -*- mode: python; coding: utf-8 -*-
 """
 FireLogger_ server-side support library for Python.
-For usage see README.md or visit the homepage_.
+For usage see ``README.txt`` or visit the `github homepage`_.
 
 .. _FireLogger: https://addons.mozilla.org/en-US/firefox/addon/11090
-.. _homepage: http://github.com/darwin/firepython
+.. _github homepage: http://github.com/darwin/firepython
 """
 
 __all__ = [
