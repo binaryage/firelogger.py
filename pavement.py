099905f41c793f190d97dee06e64691d1b6330a1
Antonin Hildebrand <antonin@hildebrand.cz>
2010-10-25 15:18:41 +0200

    fix broken pavement tasks

--- pavement.py
+++ pavement.py
@@ -30,40 +30,12 @@ SETUP_ARGS['packages'] = find_packages(exclude=['tests'])
 setup(**SETUP_ARGS)
 
 
-def get_version_from_install_rdf():
-    match = API_VERSION.search(INSTALL_RDF.bytes())
-    if match:
-        return match.groups(1)[0].strip()
-    else:
-        raise Exception('failed to determine API version from %s'
-                        % INSTALL_RDF)
-
-
 @task
 @needs(['sdist'])
 def pypi():
     """Update PyPI index and upload library sources"""
     sh('python setup.py register')
     sh('python setup.py sdist --formats=gztar,bztar,zip upload')
-
-
-@task
-def update_api_version():
-    """Resets API version in the firepython package base"""
-    assert INSTALL_RDF.exists(), \
-        "%s not found!, cannot extract version." % INSTALL_RDF
-
-    ver = get_version_from_install_rdf()
-    info('found API version %r from %s' % (ver, INSTALL_RDF))
-    init = FPY/'__init__.py'
-    old_bytes = init.bytes()
-    ver_string = PY_API_VERSION_DEF % ver
-    new_bytes = PY_API_VERSION_DEF_RE.sub(ver_string, old_bytes)
-    if old_bytes != new_bytes:
-        init.write_bytes(new_bytes)
-    else:
-        info('API version is the same, not updating...')
-
 
 @task
 def clean():
@@ -76,7 +48,7 @@ def clean():
 
 
 @task
-@needs(['update_api_version', 'minilib', 'distutils.command.sdist'])
+@needs(['minilib', 'distutils.command.sdist'])
 def sdist():
     """Combines paver minilib with setuptools' sdist"""
     pass
